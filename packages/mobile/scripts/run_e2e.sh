61b1b9d81793195cf28df462281b1d02402ce94f
Ashish Bhatia <ashishb@ashishb.net>
2019-09-11 17:18:17 -0700

    Add .travis.yml

--- packages/mobile/scripts/run_e2e.sh
+++ packages/mobile/scripts/run_e2e.sh
@@ -10,38 +10,19 @@ adb kill-server && adb start-server
 
 DEFAULT_AVD="Nexus_5X_API_28_x86"
 
-if [[ ! $($ANDROID_SDK_ROOT/emulator/emulator -list-avds | grep ^$DEFAULT_AVD$) ]]; then
+if [[ ! $(emulator -list-avds | grep ^$DEFAULT_AVD$) ]]; then
   echo "AVD $DEFAULT_AVD not installed. Pleas install it or change detox' configuration in package.json"
-  echo "You can see the list of available installed devices with $ANDROID_SDK_ROOT/emulator/emulator -list-avds"
+  echo "You can see the list of available installed devices with emulator -list-avds"
   exit 1
 fi
-
-# unlock device
-yarn dev:emulator
 
 # Just to be safe kill any process that listens on the port 'yarn start' is going to use
 lsof -t -i :8081 | xargs kill -9
 yarn start:bg
 
-bash ./scripts/unlock.sh
-adb reconnect
-if [ $? -ne 0 ]
-then
-  exit 1
-fi
-
-echo "Waiting for emulator to unlock..."
-# TODO: improve this to actually poll if the screen is unlocked
-# https://stackoverflow.com/questions/35275828/is-there-a-way-to-check-if-android-device-screen-is-locked-via-adb
-sleep 3
-echo "Emulator unlocked!"
-
-# start logs
-pidcat -t "ReactNativeJS" > e2e_pidcat_run.log & 
-
-# sometimes the emulator locks itself after boot
-# this prevents that
-bash ./scripts/unlock.sh
+# This assumes pidcat
+# # start logs
+# pidcat -t "ReactNativeJS" > e2e_pidcat_run.log & 
 
 yarn test:detox
 STATUS=$?
